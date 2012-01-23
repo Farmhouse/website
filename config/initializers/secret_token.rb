4c411c8519f3bdba698a0b362fec9967e09ed2f7
Shane Becker <veganstraightedge@gmail.com>
2012-01-23 13:28:56 -0800

    vanilla rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farmhouse::Application.config.secret_token = '2a5b449a658184a4aa464a374027a38c474ec3a0e20f856f6985e886dfde85d79166658f5dc9dd5bc1db482d922afa753ad7f6d85755b9e522a5ef224837246c'
