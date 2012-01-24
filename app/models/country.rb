c8b143f6b5deb7520bb6f26480ff8b7b121d4d60
Shane Becker <veganstraightedge@gmail.com>
2012-01-23 18:54:53 -0800

    Country select menu

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,13 @@
 class Country < ActiveRecord::Base
+  class << self
+    def options_for_select
+      countries = []
+      usa       =  Country.where(:name => "United States").first
+      canadia   =  Country.where(:name => "Canada").first
+      countries << [usa.name, usa.id]
+      countries << [canadia.name, canadia.id]
+
+      countries + Country.order("name asc").all.map{ |c| [c.name, c.id] }
+    end
+  end
 end
