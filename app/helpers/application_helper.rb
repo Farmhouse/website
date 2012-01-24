b46e5d730570afafa000aa434df596beb3e4230d
Shane Becker <veganstraightedge@gmail.com>
2012-01-23 19:47:10 -0800

    confs#show cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def link_to_twitter(thing)
+    link_to "@#{thing.twitter}", "http://twitter.com/#{thing.twitter}", :rel => "external"
+  end
+
+  def human_readable_datetime(datetime)
+    datetime.strftime("%B %e, %Y %l:%M %p")
+  end
+
+  def html5_datetime(datetime)
+    datetime.strftime("%m-%d-%yT%H:%M")
+  end
+
+  def time_tag(datetime)
+    content_tag(:time, human_readable_datetime(datetime), :datetime => html5_datetime(datetime))
+  end
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :country
   belongs_to :region
+
+  class << self
+    def options_for_select
+      Location.all.map{ |l| [l.name, l.id] }
+    end
+  end
 end
