9a42f43e429370c3151e88d0e0311745beda2874
Shane Becker <veganstraightedge@gmail.com>
2012-01-23 19:02:41 -0800

    States select menu

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,15 @@
 class State < ActiveRecord::Base
   belongs_to :country
+
+  class << self
+    def options_for_select
+      usa = Country.where(:name => "United States").first
+      canadia = Country.where(:name => "Canada").first
+
+      states    = State.where(:country_id => usa.id).order("name asc").all.map{ |c| [c.name, c.id] }
+      provinces = State.where(:country_id => canadia.id).order("name asc").all.map{ |c| [c.name, c.id] }
+
+      states + provinces
+    end
+  end
 end
