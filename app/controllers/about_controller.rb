a13362952629ff446053f830ba7e5b10c789ba75
Shane Becker <veganstraightedge@gmail.com>
2012-02-11 14:19:04 -0800

    farmhouse rules chalkboard wall photo on rules page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,6 +2,10 @@ class AboutController < ApplicationController
   def index
     @slug = "home"
   end
+  
+  def rules
+    @slug = "rules"
+  end
 
   def register
     redirect_to Conf.latest.first.registration_url
