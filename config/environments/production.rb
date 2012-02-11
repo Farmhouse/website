68f22b55de952e8497e4eb4e05f79e0aef3affcc
Shane Becker <veganstraightedge@gmail.com>
2012-02-10 17:51:24 -0800

    moved bootstrap bits to vendor/assets under twitter namespace, used =require declarations in app.css, testing heroku compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Farmhouse::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bootstrap-min.css bootstrap-responsive-min.css )
+  # config.assets.precompile += %w( bootstrap-min.css bootstrap-responsive-min.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
