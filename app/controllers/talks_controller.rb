e3fba6bf18dcbe3bc14e0ed21d75f96cf8aff346
Shane Becker <veganstraightedge@gmail.com>
2012-03-26 18:21:00 -0700

    removed year/talks route

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -6,46 +6,6 @@ class TalksController < ApplicationController
   end
 
   def index
-    @talks = Talk.all
-  end
-
-  def new
-    @talk = Talk.new
-  end
-
-  def edit
-    @talk = Talk.find(params[:id])
-  end
-
-  def create
-    @talk = Talk.new(params[:talk])
-
-    if @talk.save
-      Person.find(params[:person_id]).talks << @talk
-
-      redirect_to @talk, :notice => 'Talk was successfully created.'
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @talk = Talk.find(params[:id])
-
-    if @talk.update_attributes(params[:talk])
-      unless params[:person_id].blank?
-        @talk.people << Person.find(params[:person_id])
-      end
-
-      redirect_to @talk, :notice => 'Talk was successfully updated.'
-    else
-      render :action => "edit"
-    end
-  end
-
-  def destroy
-    @talk = Talk.find(params[:id])
-    @talk.destroy
-    redirect_to talks_path
+    redirect_to year_path(params[:year])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,10 @@ Farmhouse::Application.routes.draw do
 
   match "/conf"              => redirect("/2012")
 
-  match "/:year"             => "confs#show",     :as => "year"
-  match "/:year/photos"      => "photos#index",   :as => "year_photos"
-  match "/:year/talks"       => "talks#index",    :as => "year_talks"
-  match "/:year/talks/:slug" => "talks#show",     :as => "year_talk"
+  match "/:year"             => "confs#show",       :as => "year"
+  match "/:year/photos"      => "photos#index",     :as => "year_photos"
+  match "/:year/talks"       => "talks#index",      :as => "year_talks"
+  match "/:year/talks/:slug" => "talks#show",       :as => "year_talk"
 
-  match "/people/:slug"      => "people#show",    :as => "person_slug"
-
-
-  # adminy routes
-  unless Rails.env.development?
-    resources :confs, :links, :locations, :people, :photos, :talks, :tweets, :videos, :websites
-    resources :speakings, :only => [:destroy]
-    match "/websites/new/:whom" => "websites#new", :as => "new_person_website"
-  end
+  match "/people/:slug"      => "people#show",      :as => "person_slug"
 end
