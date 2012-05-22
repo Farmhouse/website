7ec53adb34aed7cd2b3f726978c579dd1f8a2c1a
Shane Becker <veganstraightedge@gmail.com>
2012-05-21 19:38:47 -0700

    beginning of update for fhc3

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -16,6 +16,7 @@ class AboutController < ApplicationController
   end
 
   def register
-    redirect_to Conf.latest.first.registration_url
+    # redirect_to Conf.latest.first.registration_url
+    redirect_to "https://farmhouse.eventwax.com/farmhouse-conf-3/register/"
   end
 end
