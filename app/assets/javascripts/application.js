6e3a14d8aff6a9b280d37aea74e44e02b19605d6
Shane Becker <veganstraightedge@gmail.com>
2012-12-02 00:37:49 -0800

    unvendored twootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-// = require modernizr
-// = require twitter/bootstrap
+//= require vendor/modernizr
+//= require vendor/twitter/bootstrap
+             
