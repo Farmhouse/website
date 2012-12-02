0d02ba3ca272fa1c9f20b3eb7422168120e17809
Shane Becker <veganstraightedge@gmail.com>
2012-12-01 23:30:06 -0800

    updated podcasts#feed to use Episode objects

--- app/controllers/podcast_controller.rb
+++ app/controllers/podcast_controller.rb
@@ -1,15 +1,17 @@
 class PodcastController < ApplicationController
   def index
-    @slug = "podcast"
-    @title = "The Farmhouse Podcast - Subscribe Now"
+    @slug     = "podcast"
+    @title    = "The Farmhouse Podcast - Subscribe Now"
     @episodes = Episode.all
   end
 
   def show
     @episode = Episode.find(params[:id])
+    @title   = "The Farmhouse Podcast : S01E#{@episode.episode} : #{@episode.title}"
   end
 
   def feed
+    @episodes = Episode.all
     render :template => "/podcast/feed.xml",  :layout => false
   end
 end
