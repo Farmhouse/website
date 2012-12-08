72806456d93dbf6a5c836fd5fc981a6ee3d8d567
Shane Becker <veganstraightedge@gmail.com>
2012-12-08 11:51:51 -0800

    removed labs

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -2,14 +2,4 @@ class LabsController < ApplicationController
   def index
     @lab = Lab.new
   end
-
-  def create
-    @lab = Lab.new(params[:lab])
-
-    if @lab.save
-      redirect_to labs_path, :notice => "Lab was successfully created."
-    else
-      render :action => "index"
-    end
-  end
 end
