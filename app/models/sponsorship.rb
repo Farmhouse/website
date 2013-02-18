3934707ac052af368120a486716484c529984697
Shane Becker <veganstraightedge@gmail.com>
2013-02-17 17:18:42 -0800

    added scopes to sponsorships

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -3,4 +3,10 @@ class Sponsorship < ActiveRecord::Base
 
   belongs_to :conf
   belongs_to :sponsor
+
+  scope :party,       where(:level => :party)
+  scope :standard,    where(:level => :standard)
+  scope :goodie,      where(:level => :goodie)
+  scope :food,        where(:level => :food)
+  scope :scholarship, where(:level => :scholarship)
 end
