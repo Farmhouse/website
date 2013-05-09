d35408fc3723c1c12e28275211d6dc2e3abbe46f
Shane Becker <veganstraightedge@gmail.com>
2013-05-09 00:53:18 -0700

    simplified conf alumni view

--- app/controllers/confs_controller.rb
+++ app/controllers/confs_controller.rb
@@ -4,6 +4,26 @@ class ConfsController < ApplicationController
     @conf     = Conf.find params[:id]
     @sub_slug = "conf-#{@conf.id}"
     @title    = @conf.name
+
+
+    alumni_names = case @conf.id
+    when 2
+      ["Sean Bonner", "Eli Duke"]
+    when 3
+      ["Sean Bonner", "Tara Brown", "Mitch Artman"]
+    when 4
+      ["Eric Gradman", "Megan Dean", "Mitch Artman", "Tara Brown"]
+    when 5
+      ["Eli Duke", "Evan Phoenix", "Leah Silber", "Sarah Mei", "Tj Nelson Jr", "Greg Bennick", "Mitch Artman", "Tara Brown", "Ron Evans", "willowbl00"]
+    else
+      []
+    end
+
+    @alumni = []
+    alumni_names.each do |name|
+      @alumni << Person.where(:name => name).first
+    end
+
   end
 
   def code_of_conduct
