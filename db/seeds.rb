3eb176ab58773ec5e0d1ba2d47bf6a3feebee778
Shane Becker <veganstraightedge@gmail.com>
2013-05-10 10:39:31 -0700

    removed anonymous speaker from fhc5

--- db/seeds.rb
+++ db/seeds.rb
@@ -843,17 +843,6 @@ people_5 = [
     }
   },
   {
-    :name => "Anonymous",
-    :bio  => %Q{
-      <p>He worked on some thing.</p>
-    },
-    :talk => {
-      :slug        => "TBA",
-      :title       => "TBA",
-      :description => %Q{ <p></p> },
-    }
-  },
-  {
     :name => "Claire Evans",
     :bio  => %Q{
       <p>Claire L. Evans is a writer and artist working in Los Angeles, California. Her &ldquo;day job&rdquo; is as the singer and co-author of the conceptual disco-pop band YACHT, which recently released its fifth album Shangri-La, on DFA Records. A science journalist and science fiction critic, her blog, Universe, addressing synchronies between art, science, technology, and the cultural world, was recently anthologized in Best Science Writing on the Web 2012 (Scientific American Books/Farrar, Straus & Giroux), and she regularly participates in panels, conferences, and screenings on the subject of science and culture. She has performed earnestly cosmic presentations at the Kitchen, MoMA PS1, and the Hirshhorn Museum, spoken about extraterrestrial life at the Rubin Museum&rsquo;s BRAINWAVE series, and co-authored a book on interdisciplinarity in the arts, NA/SA: New Art Science Affinities. A collected book of her essays, High Frontiers, is now available from Publication Studio.</p>
