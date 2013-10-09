44f1f96a3251b7307b21bec56dceb3382303079a
Shane Becker <veganstraightedge@gmail.com>
2013-10-08 17:19:35 -0700

    updated Claire Evans' info

--- db/seeds.rb
+++ db/seeds.rb
@@ -941,12 +941,14 @@ people_5 = [
   {
     :name => "Claire Evans",
     :bio  => %Q{
-      <p>Claire L. Evans is a writer and artist working in Los Angeles, California. Her &ldquo;day job&rdquo; is as the singer and co-author of the conceptual disco-pop band YACHT, which recently released its fifth album Shangri-La, on DFA Records. A science journalist and science fiction critic, her blog, Universe, addressing synchronies between art, science, technology, and the cultural world, was recently anthologized in Best Science Writing on the Web 2012 (Scientific American Books/Farrar, Straus & Giroux), and she regularly participates in panels, conferences, and screenings on the subject of science and culture. She has performed earnestly cosmic presentations at the Kitchen, MoMA PS1, and the Hirshhorn Museum, spoken about extraterrestrial life at the Rubin Museum&rsquo;s BRAINWAVE series, and co-authored a book on interdisciplinarity in the arts, NA/SA: New Art Science Affinities. A collected book of her essays, High Frontiers, is now available from Publication Studio.</p>
+      <p><a href="http://www.clairelevans.com" title="Claire L. Evans &mdash; STAMP OUT REALITY">Claire L. Evans</a> is a writer and artist working in Los Angeles, California. Her &ldquo;day job&rdquo; is as the singer and co-author of the conceptual disco-pop band <a href="http://teamyacht.com" title="YACHT &#9651; Hello">YACHT</a>. A science journalist and science-fiction critic, she is a regular contributor to Aeon Magazine, Vice, Motherboard, and Grantland, and is the editor-in-chief of <a href="http://omnireboot.com" title="Omni Reboot">OMNI Reboot</a>.</p>
+      <p>Her writing has been was anthologized in <a href="http://www.amazon.com/Best-Science-Writing-Online-Laboratory/dp/B00DJZIEHE" title="The Best Science Writing Online 2012 (Open Laboratory): Jennifer Ouellette, Bora Zivkovic: 0971486646597: Amazon.com: Books">Best Science Writing Online 2012</a> (Scientific American Books/Farrar, Straus &amp; Giroux), and she regularly participates in panels, conferences, and screenings on the subject of science and culture. She has performed earnestly cosmic presentations at the Kitchen, MoMA PS1, and the Hirshhorn Museum, spoken about extraterrestrial life at the Rubin Museum&rsquo;s BRAINWAVE series, and co-authored a book on interdisciplinarity in the arts, <a href="http://millergallery.cfa.cmu.edu/nasabook/" title="New Art/Science Affinities book &gt;Miller Gallery at Carnegie Mellon University">NA/SA: New Art Science Affinities</a>. </p>
+      <p>A collected book of her essays, <a href="http://www.publicationstudio.biz/books/215" title="Publication Studio  &#8211;  Books  &#8211;  215">High Frontiers</a>, is now available from Publication Studio.</p>
     },
     :talk => {
-      :slug        => "claire-evans-tba",
-      :title       => "TBA",
-      :description => %Q{ <p></p> },
+      :slug        => "the-collapse-of-love-in-the-age-of-machines",
+      :title       => "The Collapse of Love in the Age of Machines",
+      :description => %Q{ <p>When it comes to love, we might all be Luddites. Wouldn&rsquo;t we smash the machine that told us precisely where and when our former paramours found someone new? Unfortunately, the global index that contains every gossamer byte of the love letters we're always unwittingly writing as we make contact across the web is quite unsmashable. We are at a fundamental cultural tipping point in how we deal with love, courtship&mdashand inevitably, heartbreak. Is this the collapse of love as we know it? How do we renegotiate intimacy in the Internet age?</p> },
     }
   },
   {
