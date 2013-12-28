b314860d4ee2843c246d20ecb8b230b0c47d3197
Shane Becker <veganstraightedge@gmail.com>
2013-12-28 14:39:27 -0800

    added registration button to barn talks 1 page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -19,6 +19,6 @@ class AboutController < ApplicationController
   end
 
   def register
-    redirect_to "https://farmhouse.eventwax.com/farmhouse-conf-5/register/"
+    redirect_to "https://www.eventbrite.com/e/barn-talk-1-warren-ellis-tickets-9965417833"
   end
 end
