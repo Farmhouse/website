00f75cc588396174d320ca6127e058d3bb9bf55b
Shane Becker <veganstraightedge@gmail.com>
2014-03-23 14:52:37 -0700

    added scripts js to precompile prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Farmhouse::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(new/vendor/responsive-nav.min.js)
+  config.assets.precompile += %w(new/vendor/responsive-nav.min.js new/scripts.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
